419874b63a210e4f1702584a4aad9254244b1502
Anmol Singh <Anmol_singh@live.no>
2023-08-23 16:31:41 +0200

    Add accordians

--- spa/web/src/pages/TreatmentPage.tsx
+++ spa/web/src/pages/TreatmentPage.tsx
@@ -1,5 +1,5 @@
 import { useAccount, useMsal } from '@azure/msal-react';
-import { Box, Button, Flex, Heading, Text, Tooltip } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Text, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon, } from '@chakra-ui/react';
 import { Guest, Room } from 'cms-types';
 import { useEffect } from 'react';
 import useAccessToken from '../auth/useAccessToken';
@@ -59,13 +59,52 @@ const TreatmentPage = () => {
       height="100vh"
       alignContent="center"
       justifyContent="center"
+      flexDirection="column"
       backgroundColor="#f0f0f0"
     >
       <Box m="0 auto">
-        <Heading as="h1" textAlign="center" fontSize="5xl" mt="100px">
+        <Heading as="h1" textAlign="center" fontSize="2xl" mt="100px">
           What treatments would you like to book, {account?.name}?
         </Heading>
       </Box>
+      <Box m="0 auto" p={4} marginTop={4}>
+        <Accordion>
+        <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as="span" flex='1' textAlign='left'>
+          Section 1 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as="span" flex='1' textAlign='left'>
+          Section 2 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+        </Accordion>
+
+  </Box>
     </Flex>
   );
 };
