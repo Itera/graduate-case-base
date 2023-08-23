5b1e2c1dbf01c0879026aa31e1395dd2b66949a9
Håkon Skogen <84183241+Handere@users.noreply.github.com>
2023-08-23 16:48:18 +0200

    fix: bug

--- spa/api/Explore.Spa/Models/Booking.cs
+++ spa/api/Explore.Spa/Models/Booking.cs
@@ -4,8 +4,8 @@ namespace Explore.Spa.Models;
 
 public class Booking
 {
-    public Treatment TreatmentName { get; init; } = string.Empty;
-    public GuestResponse GuestName { get; init} = GuestResponse.Empty;
+    public Treatment TreatmentName { get; init; }
+    public GuestResponse GuestName { get; init; } = null;
     public DateTime StartDateTime { get; set; } = DateTime.UtcNow;
     public DateTime EndDateTime { get; set; } = DateTime.UtcNow;
 }
