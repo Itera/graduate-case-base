04cb9b66b48371d0873ed87b18f7894200bb76a0
Nathan Norton <nathan.norton@itera.com>
2023-08-23 22:19:18 +0200

    cards are clickable

--- restaurant/web/src/pages/RestaurantOverview.tsx
+++ restaurant/web/src/pages/RestaurantOverview.tsx
@@ -8,8 +8,11 @@ import {
   Stack,
   CardFooter,
   Button,
-  Heading
+  Heading,
+  Link as ChakraLink,
+  LinkProps
 } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 const RestaurantOverview = () => {
   return (
@@ -21,129 +24,137 @@ const RestaurantOverview = () => {
       backgroundColor="#f0f0f0"
     >
       <Box>
-        <Card
-          direction={{ base: 'column', sm: 'row' }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: '100%', sm: '200px' }}
-            src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-            alt="Caffe Latte"
-          />
+        <ChakraLink as={ReactRouterLink} to="/restaurant">
+          <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow="hidden"
+            variant="outline"
+          >
+            <Image
+              objectFit="cover"
+              maxW={{ base: '100%', sm: '200px' }}
+              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+              alt="Caffe Latte"
+            />
 
-          <Stack>
-            <CardBody>
-              <Heading size="md">The Arctic Penguin</Heading>
+            <Stack>
+              <CardBody>
+                <Heading size="md">The Arctic Penguin</Heading>
 
-              <Text py="2">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
-                recusandae neque, velit praesentium numquam ducimus totam atque
-                illo nam qui!
-              </Text>
-            </CardBody>
+                <Text py="2">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+                  recusandae neque, velit praesentium numquam ducimus totam
+                  atque illo nam qui!
+                </Text>
+              </CardBody>
 
-            <CardFooter>
-              <Button variant="outline" color="#FFB46D">
-                Book
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
+              <CardFooter>
+                <Button variant="outline" color="#FFB46D">
+                  Book
+                </Button>
+              </CardFooter>
+            </Stack>
+          </Card>
+        </ChakraLink>
 
-        <Card
-          direction={{ base: 'column', sm: 'row' }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: '100%', sm: '200px' }}
-            src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-            alt="Caffe Latte"
-          />
+        <ChakraLink as={ReactRouterLink} to="/restaurant">
+          <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow="hidden"
+            variant="outline"
+          >
+            <Image
+              objectFit="cover"
+              maxW={{ base: '100%', sm: '200px' }}
+              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+              alt="Caffe Latte"
+            />
 
-          <Stack>
-            <CardBody>
-              <Heading size="md">The South Pole</Heading>
+            <Stack>
+              <CardBody>
+                <Heading size="md">The South Pole</Heading>
 
-              <Text py="2">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
-                recusandae neque, velit praesentium numquam ducimus totam atque
-                illo nam qui!
-              </Text>
-            </CardBody>
+                <Text py="2">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+                  recusandae neque, velit praesentium numquam ducimus totam
+                  atque illo nam qui!
+                </Text>
+              </CardBody>
 
-            <CardFooter>
-              <Button variant="outline" color="#FFB46D">
-                Book
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
+              <CardFooter>
+                <Button variant="outline" color="#FFB46D">
+                  Book
+                </Button>
+              </CardFooter>
+            </Stack>
+          </Card>
+        </ChakraLink>
 
-        <Card
-          direction={{ base: 'column', sm: 'row' }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: '100%', sm: '200px' }}
-            src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-            alt="Caffe Latte"
-          />
+        <ChakraLink as={ReactRouterLink} to="/restaurant">
+          <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow="hidden"
+            variant="outline"
+          >
+            <Image
+              objectFit="cover"
+              maxW={{ base: '100%', sm: '200px' }}
+              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+              alt="Caffe Latte"
+            />
 
-          <Stack>
-            <CardBody>
-              <Heading size="md">The North Pole</Heading>
+            <Stack>
+              <CardBody>
+                <Heading size="md">The North Pole</Heading>
 
-              <Text py="2">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
-                recusandae neque, velit praesentium numquam ducimus totam atque
-                illo nam qui!
-              </Text>
-            </CardBody>
+                <Text py="2">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+                  recusandae neque, velit praesentium numquam ducimus totam
+                  atque illo nam qui!
+                </Text>
+              </CardBody>
 
-            <CardFooter>
-              <Button variant="outline" color="#FFB46D">
-                Book
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
+              <CardFooter>
+                <Button variant="outline" color="#FFB46D">
+                  Book
+                </Button>
+              </CardFooter>
+            </Stack>
+          </Card>
+        </ChakraLink>
 
-        <Card
-          direction={{ base: 'column', sm: 'row' }}
-          overflow="hidden"
-          variant="outline"
-        >
-          <Image
-            objectFit="cover"
-            maxW={{ base: '100%', sm: '200px' }}
-            src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-            alt="Caffe Latte"
-          />
+        <ChakraLink as={ReactRouterLink} to="/restaurant">
+          <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow="hidden"
+            variant="outline"
+          >
+            <Image
+              objectFit="cover"
+              maxW={{ base: '100%', sm: '200px' }}
+              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+              alt="Caffe Latte"
+            />
 
-          <Stack>
-            <CardBody>
-              <Heading size="md">The South Pole</Heading>
+            <Stack>
+              <CardBody>
+                <Heading size="md">The South Pole</Heading>
 
-              <Text py="2">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
-                recusandae neque, velit praesentium numquam ducimus totam atque
-                illo nam qui!
-              </Text>
-            </CardBody>
+                <Text py="2">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+                  recusandae neque, velit praesentium numquam ducimus totam
+                  atque illo nam qui!
+                </Text>
+              </CardBody>
 
-            <CardFooter>
-              <Button variant="outline" color="#FFB46D">
-                Book
-              </Button>
-            </CardFooter>
-          </Stack>
-        </Card>
+              <CardFooter>
+                <Button variant="outline" color="#FFB46D">
+                  Book
+                </Button>
+              </CardFooter>
+            </Stack>
+          </Card>
+        </ChakraLink>
       </Box>
     </Flex>
   );
