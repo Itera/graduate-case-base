6c70c6298bf0fcbbb487bf84bb1f5920ff1ac477
Peter Kristiansen <pet.petkri@gmail.com>
2023-08-24 09:10:59 +0200

    feature: add reserve table

--- restaurant/web/src/pages/RestaurantHome.tsx
+++ restaurant/web/src/pages/RestaurantHome.tsx
@@ -1,7 +1,8 @@
-import { Box, Button, Heading, Stack, Text } from "@chakra-ui/react"
+import { Box, Button, Heading, Link, Stack, Text } from "@chakra-ui/react"
 import data from "./menu.json";
 import Menu from "./Menu";
 import theme from "../theme";
+import { Link as ReactLink } from "react-router-dom";
 
 
 
@@ -23,9 +24,12 @@ const RestaurantHome = () => {
                     facilisi. Sed euismod mauris vel sapien tincidunt, vel bibendum sapien
                     bibendum. Nulla facilisi.
                 </Text>
-                <Button mt={4} bgColor={theme.colors["explore-yellow"].main} textColor={theme.colors["explore-blue"].main}>
+                
+                <Link as={ReactLink} to="/restaurant/1/reserve">
+                    <Button mt={4} bgColor={theme.colors["explore-yellow"].main} textColor={theme.colors["explore-blue"].main}>
                     Book a Table
-                </Button>
+                </Button></Link>
+                
             </Box>
   
 
